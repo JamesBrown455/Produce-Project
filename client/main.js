2b31c8b1a8e67671818b8580c97e0f6d60f943ef
James Brown <2yankeefans2@gmail.com>
2017-02-15 19:33:21 -0500

    Set up basic nav system

--- client/main.js
+++ client/main.js
@@ -1,22 +1,17 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import '../imports/ui/body.js';
 
-import './main.html';
+Router.configure({
+   layoutTemplate: 'body'  //can be any template name
+ });
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+Router.map(function () {
+  this.route('home', {
+    path: '/',
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+  this.route('about');
+  this.route('account');
+  this.route('products');
+  this.route('partners');
 });
